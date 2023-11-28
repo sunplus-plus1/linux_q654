613fb3b3375d90ec648d1a32d29ee8000993c25b
Qin Jian <qinjian@sunmedia.com.cn>
2023-11-28 15:17:32 +0800

    [drivers][crypto] speedup chacha20

--- drivers/crypto/sunplus/sp-aes.c
+++ drivers/crypto/sunplus/sp-aes.c
@@ -13,6 +13,7 @@
 #include <linux/types.h>
 #include <linux/crypto.h>
 #include <linux/delay.h>
+#include <linux/dma-mapping.h>
 #include <crypto/aes.h>
 #include <crypto/chacha.h>
 #include "sp-crypto.h"
@@ -59,17 +60,20 @@ static int sp_aes_set_key(struct crypto_tfm *tfm, const u8 *in_key,
 	return 0;
 }
 
-static void sp_aes_exec(struct crypto_tfm *tfm, u8 *out, const u8 *in, u32 size, u32 mode)
+static void sp_aes_exec(struct crypto_tfm *tfm, uintptr_t dst, uintptr_t src, u32 size, u32 mode)
 {
 	struct sp_crypto_reg *reg = sp_crypto_dev()->reg;
 	struct sp_crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
+	int b = size < CHACHA_BLOCK_SIZE;
 
-	SP_CRYPTO_DBG("[%s:%d] %px %px %px %px %u %u\n", __FUNCTION__, __LINE__, reg, ctx, in, out, mode, ctx->blksize);
+	SP_CRYPTO_DBG("[%04x:%02x] %08lx %08lx %u\n", mode, ctx->blksize, src, dst, size);
 	mutex_lock(&sp_aes.lock);
 
 	sp_aes.done = false;
-	//dump_buf(in, size);
-	memcpy(sp_aes.va, in, size);
+	if (b) {
+		memcpy(sp_aes.va, (void *)src, size);
+		//dump_buf(sp_aes.va, size);
+	}
 	memcpy(sp_aes.va + CHACHA_BLOCK_SIZE, ctx->key, ctx->key_length);
 	if (ctx->iv) {
 		memcpy(sp_aes.va + CHACHA_BLOCK_SIZE + AES_KEYSIZE_256, (void *)ctx->iv, CHACHA_IV_SIZE);
@@ -81,28 +85,28 @@ static void sp_aes_exec(struct crypto_tfm *tfm, u8 *out, const u8 *in, u32 size,
 	W(AESPAR1, ctx->iv); // iv
 	ctx->iv = 0; // initial iv @ ctx 1st call hw
 	W(AESPAR2, sp_aes.pa + CHACHA_BLOCK_SIZE); // kptr
-	W(AESSPTR, sp_aes.pa);
-	W(AESDPTR, sp_aes.pa);
-	W(AESDMACS, SEC_DMA_SIZE(ctx->blksize) | SEC_DMA_ENABLE);
+	W(AESSPTR, b ? sp_aes.pa : src);
+	W(AESDPTR, b ? sp_aes.pa : dst);
+	W(AESDMACS, SEC_DMA_SIZE(b ? CHACHA_BLOCK_SIZE : size) | SEC_DMA_ENABLE);
 	wait_event_interruptible_timeout(sp_aes.wait, sp_aes.done, 60*HZ);
-	//while (!(R(SECIF) & AES_DMA_IF));
-	//W(SECIF, AES_DMA_IF); // clear aes dma finish flag
 
 	SP_CRYPTO_TRACE();
-	memcpy(out, sp_aes.va, size);
-	//dump_buf(out, size);
+	if (b) {
+		//dump_buf(sp_aes.va, size);
+		memcpy((void *)dst, sp_aes.va, size);
+	}
 
 	mutex_unlock(&sp_aes.lock);
 }
 
 static void sp_aes_encrypt(struct crypto_tfm *tfm, u8 *out, const u8 *in)
 {
-	return sp_aes_exec(tfm, out, in, AES_BLOCK_SIZE, M_AES_ECB | M_ENC);
+	return sp_aes_exec(tfm, (uintptr_t)out, (uintptr_t)in, AES_BLOCK_SIZE, M_AES_ECB | M_ENC);
 }
 
 static void sp_aes_decrypt(struct crypto_tfm *tfm, u8 *out, const u8 *in)
 {
-	return sp_aes_exec(tfm, out, in, AES_BLOCK_SIZE, M_AES_ECB | M_DEC);
+	return sp_aes_exec(tfm, (uintptr_t)out, (uintptr_t)in, AES_BLOCK_SIZE, M_AES_ECB | M_DEC);
 }
 
 static int sp_chacha_set_key(struct crypto_skcipher *tfm,
@@ -113,37 +117,46 @@ static int sp_chacha_set_key(struct crypto_skcipher *tfm,
 
 static int sp_chacha_crypt(struct skcipher_request *req, u32 mode)
 {
+	struct device *dev = sp_crypto_dev()->device;
 	struct crypto_skcipher *tfm = crypto_skcipher_reqtfm(req);
 	struct sp_crypto_aes_ctx *ctx = crypto_skcipher_ctx(tfm);
 	struct skcipher_walk walk;
+	unsigned int nbytes;
 	int err;
 
 	err = skcipher_walk_virt(&walk, req, false);
 	ctx->iv = (uintptr_t)req->iv;
 	ctx->blksize = CHACHA_BLOCK_SIZE;
 
-	while (walk.nbytes > 0) {
-		unsigned int nbytes = walk.nbytes;
-		u8* src = walk.src.virt.addr;
-		u8* dst = walk.dst.virt.addr;
-		u32 bytes;
+	while ((nbytes = walk.nbytes) > 0) {
+		uintptr_t src = (uintptr_t)walk.src.virt.addr;
+		uintptr_t dst = (uintptr_t)walk.dst.virt.addr;
+		u32 bytes = nbytes - (nbytes % ctx->blksize) ?: nbytes;
 
-		if (nbytes < walk.total)
-			nbytes = round_down(nbytes, CHACHA_BLOCK_SIZE);
-
-		//printk("%u %u %u\n", walk.nbytes, walk.total, nbytes);
-		bytes = nbytes;
-		while (bytes >= CHACHA_BLOCK_SIZE) {
-			sp_aes_exec(&tfm->base, dst, src, CHACHA_BLOCK_SIZE, mode);
-			bytes -= CHACHA_BLOCK_SIZE;
-			dst += CHACHA_BLOCK_SIZE;
-			src += CHACHA_BLOCK_SIZE;
+		SP_CRYPTO_DBG("%u %u %u\n", nbytes, walk.total, bytes);
+		if (bytes >= CHACHA_BLOCK_SIZE) {
+			if (src != dst) {
+				src = dma_map_single(dev, (void *)src, bytes, DMA_TO_DEVICE);
+				dst = dma_map_single(dev, (void *)dst, bytes, DMA_FROM_DEVICE);
+			} else {
+				src = dma_map_single(dev, (void *)src, bytes, DMA_BIDIRECTIONAL);
+				dst = src;
+			}
 		}
-		if (bytes) {
-			sp_aes_exec(&tfm->base, dst, src, bytes, mode);
+		sp_aes_exec(&tfm->base, dst, src, bytes, mode);
+		if (bytes >= CHACHA_BLOCK_SIZE) {
+			if (src != dst) {
+				dma_unmap_single(dev, src, bytes, DMA_TO_DEVICE);
+				dma_unmap_single(dev, dst, bytes, DMA_FROM_DEVICE);
+			} else {
+				dma_unmap_single(dev, src, bytes, DMA_BIDIRECTIONAL);
+			}
 		}
 
-		err = skcipher_walk_done(&walk, walk.nbytes - nbytes);
+		nbytes -= bytes;
+		err = skcipher_walk_done(&walk, nbytes);
+		if (err)
+			break;
 	}
 
 	return err;
