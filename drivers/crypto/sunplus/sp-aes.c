d0b1dfd04f6d7b56bfdccfb2cbc7351f8c69eec1
Qin Jian <qinjian@sunmedia.com.cn>
2023-12-18 09:56:12 +0800

    [drivers][crypto] refine crypto driver; add SHA-2/POLY1305 hash

--- drivers/crypto/sunplus/sp-aes.c
+++ drivers/crypto/sunplus/sp-aes.c
@@ -17,15 +17,13 @@
 #include <crypto/chacha.h>
 #include "sp-crypto.h"
 
-#define SEC_DMA_ENABLE	(1 << 0)
-#define SEC_DMA_SIZE(x) ((x) << 16)
 #define M_KEYLEN(x)     (((x) >> 2) << 16)
 
 #define WORK_BUF_SIZE	(CHACHA_BLOCK_SIZE + AES_KEYSIZE_256 + CHACHA_IV_SIZE)
 #define IV_OFFSET	(CHACHA_BLOCK_SIZE + AES_KEYSIZE_256)
 #define KEY_OFFSET	(CHACHA_BLOCK_SIZE)
 
-struct sp_crypto_aes_ctx {
+struct sp_aes_ctx {
 	u8 key[AES_KEYSIZE_256];
 	u32 mode;
 	u32 gctr;
@@ -40,14 +38,14 @@ struct sp_aes_priv {
 	struct mutex lock;
 } sp_aes;
 
-extern struct sp_crypto_dev *sp_crypto_dev(void);
+static struct sp_crypto_dev *crypto;
 static struct device *dev;
 static struct sp_crypto_reg *reg;
 
 static int sp_aes_set_key(struct crypto_tfm *tfm, const u8 *in_key,
 		unsigned int key_len)
 {
-	struct sp_crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
+	struct sp_aes_ctx *ctx = crypto_tfm_ctx(tfm);
 	int err;
 
 	SP_CRYPTO_DBG("[%s:%d] %px %px %u", __FUNCTION__, __LINE__, ctx, in_key, key_len);
@@ -73,7 +71,7 @@ static void sp_aes_hw_exec(u32 src, u32 dst, u32 bytes)
 
 static void sp_aes_exec(struct crypto_tfm *tfm, u8 *out, const u8 *in, u32 mode)
 {
-	struct sp_crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
+	struct sp_aes_ctx *ctx = crypto_tfm_ctx(tfm);
 	const u32 bytes = AES_BLOCK_SIZE;
 
 	//SP_CRYPTO_DBG("[%s:%d] %px %px %px %u %u\n", __FUNCTION__, __LINE__, ctx, in, out, mode, blksize);
@@ -116,12 +114,11 @@ static int sp_skcipher_set_key(struct crypto_skcipher *tfm,
 static int sp_skcipher_crypt(struct skcipher_request *req, u32 mode)
 {
 	struct crypto_skcipher *tfm = crypto_skcipher_reqtfm(req);
-	struct sp_crypto_aes_ctx *ctx = crypto_skcipher_ctx(tfm);
+	struct sp_aes_ctx *ctx = crypto_skcipher_ctx(tfm);
 	const unsigned int ivsize = crypto_skcipher_ivsize(tfm);
 	const unsigned int blksize = crypto_skcipher_chunksize(tfm);
 	struct skcipher_walk walk;
 	unsigned int nbytes;
-	u32 fix_iv; /* CHACHA20 fix hw iv issue */
 	int err;
 
 	mutex_lock(&sp_aes.lock);
@@ -138,15 +135,15 @@ static int sp_skcipher_crypt(struct skcipher_request *req, u32 mode)
 	if (ivsize) {
 		memcpy(IV, req->iv, ivsize);
 		W(AESPAR1, sp_aes.pa + IV_OFFSET);
-		fix_iv = IV[1];
 	}
 
 	err = skcipher_walk_virt(&walk, req, false);
 	while (!err && (nbytes = walk.nbytes) > 0) {
 		void *src = walk.src.virt.addr;
 		void *dst = walk.dst.virt.addr;
-		u32 bytes = nbytes - (nbytes % blksize) ?: nbytes;
+		u32 bytes = nbytes & ~(blksize - 1) ?: nbytes;
 
+do_blocks:
 		if (bytes > blksize) {
 			dma_addr_t src_pa, dst_pa;
 
@@ -165,6 +162,13 @@ static int sp_skcipher_crypt(struct skcipher_request *req, u32 mode)
 				src_pa = dma_map_single(dev, src, bytes, DMA_BIDIRECTIONAL);
 				dst_pa = src_pa;
 			}
+			if (unlikely(dma_mapping_error(dev, dst_pa))) {
+				if (unlikely(!dma_mapping_error(dev, src_pa)))
+					dma_unmap_single(dev, src_pa, bytes, DMA_TO_DEVICE);
+				bytes -= blksize;
+				dev_warn_once(dev, "dma_mapping_error, decrease bytes & retry!\n");
+				goto do_blocks;
+			}
 
 			sp_aes_hw_exec(src_pa, dst_pa, bytes);
 
@@ -175,17 +179,18 @@ static int sp_skcipher_crypt(struct skcipher_request *req, u32 mode)
 				dma_unmap_single(dev, src_pa, bytes, DMA_BIDIRECTIONAL);
 			}
 		} else {
-			//dump_buf(src, bytes);
 			memcpy(sp_aes.va, src, bytes);
+			//dump_buf(sp_aes.va, bytes);
 
 			sp_aes_hw_exec(sp_aes.pa, sp_aes.pa, blksize);
 
+			//dump_buf(sp_aes.va, bytes);
 			memcpy(dst, sp_aes.va, bytes);
-			//dump_buf(dst, bytes);
 		}
 
+		/* CHACHA20 fix hw iv issue */
 		if (ctx->mode == M_CHACHA20 && !IV[0])
-			IV[1] = fix_iv;
+			IV[1]--;
 
 		nbytes -= bytes;
 		err = skcipher_walk_done(&walk, nbytes);
@@ -205,7 +210,7 @@ static int sp_skcipher_crypt(struct skcipher_request *req, u32 mode)
 
 static int sp_cra_init(struct crypto_tfm *tfm)
 {
-	struct sp_crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
+	struct sp_aes_ctx *ctx = crypto_tfm_ctx(tfm);
 
 	ctx->gctr = (ctx->mode ? 32 : 128) << 24; // 2nd call from gcm.c hack
 	ctx->mode = crypto_tfm_alg_priority(tfm) & M_MMASK;
@@ -229,7 +234,7 @@ static struct crypto_alg sp_aes_generic_alg = {
 	.cra_priority		=	400,
 	.cra_flags		=	CRYPTO_ALG_TYPE_CIPHER,
 	.cra_blocksize		=	AES_BLOCK_SIZE,
-	.cra_ctxsize		=	sizeof(struct sp_crypto_aes_ctx),
+	.cra_ctxsize		=	sizeof(struct sp_aes_ctx),
 	.cra_module		=	THIS_MODULE,
 	.cra_u			=	{
 		.cipher = {
@@ -248,8 +253,8 @@ static struct skcipher_alg algs[] = {
 		.base.cra_driver_name	= "sp-aes-ecb",
 		.base.cra_priority	= 0x200 | M_AES_ECB,
 		.base.cra_blocksize	= AES_BLOCK_SIZE,
-		//.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
-		.base.cra_ctxsize	= sizeof(struct sp_crypto_aes_ctx),
+		.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
+		.base.cra_ctxsize	= sizeof(struct sp_aes_ctx),
 		.base.cra_init		= sp_cra_init,
 		.base.cra_module	= THIS_MODULE,
 
@@ -266,8 +271,8 @@ static struct skcipher_alg algs[] = {
 		.base.cra_driver_name	= "sp-aes-cbc",
 		.base.cra_priority	= 0x200 | M_AES_CBC,
 		.base.cra_blocksize	= AES_BLOCK_SIZE,
-		//.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
-		.base.cra_ctxsize	= sizeof(struct sp_crypto_aes_ctx),
+		.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
+		.base.cra_ctxsize	= sizeof(struct sp_aes_ctx),
 		.base.cra_init		= sp_cra_init,
 		.base.cra_module	= THIS_MODULE,
 
@@ -284,8 +289,8 @@ static struct skcipher_alg algs[] = {
 		.base.cra_driver_name	= "sp-aes-ctr",
 		.base.cra_priority	= 0x200 | M_AES_CTR,
 		.base.cra_blocksize	= 1,
-		//.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
-		.base.cra_ctxsize	= sizeof(struct sp_crypto_aes_ctx),
+		.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
+		.base.cra_ctxsize	= sizeof(struct sp_aes_ctx),
 		.base.cra_init		= sp_cra_init,
 		.base.cra_module	= THIS_MODULE,
 
@@ -302,8 +307,8 @@ static struct skcipher_alg algs[] = {
 		.base.cra_driver_name	= "sp-chacha20",
 		.base.cra_priority	= 0x200 | M_CHACHA20,
 		.base.cra_blocksize	= 1,
-		//.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
-		.base.cra_ctxsize	= sizeof(struct sp_crypto_aes_ctx),
+		.base.cra_alignmask	= AES_BLOCK_SIZE - 1,
+		.base.cra_ctxsize	= sizeof(struct sp_aes_ctx),
 		.base.cra_init		= sp_cra_init,
 		.base.cra_module	= THIS_MODULE,
 
@@ -320,8 +325,9 @@ static struct skcipher_alg algs[] = {
 int sp_aes_init(void)
 {
 	if (!dev) {
-		dev = sp_crypto_dev()->device;
-		reg = sp_crypto_dev()->reg;
+		crypto = sp_crypto_alloc_dev(SP_CRYPTO_AES);
+		dev = crypto->device;
+		reg = crypto->reg;
 		init_waitqueue_head(&sp_aes.wait);
 		mutex_init(&sp_aes.lock);
 		sp_aes.va = dma_alloc_coherent(dev, WORK_BUF_SIZE, &sp_aes.pa, GFP_KERNEL);
