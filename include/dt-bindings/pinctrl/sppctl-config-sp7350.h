84fc87e60a7332ded9510c072da00df30f5108f6
yb.leng <yb.leng@sunmedia.com.cn>
2024-01-03 14:16:56 +0800

    [dt-bindings][pinctrl]delete unnecessary copyright notice

--- include/dt-bindings/pinctrl/sppctl-config-sp7350.h
+++ include/dt-bindings/pinctrl/sppctl-config-sp7350.h
@@ -1,20 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0
- *
- * SP7021 pinmux pinctrl bindings.
- * Copyright (C) 2023 Sunplus Tech./Tibbo Tech.
- * Author: yb.leng <yb.leng@sunmedia.com.cn>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
+/* SPDX-License-Identifier: GPL-2.0 */
 
 #ifndef __DT_BINDINGS_PINCTRL_SPPCTRL_CONFIG_H
 #define __DT_BINDINGS_PINCTRL_SPPCTRL_CONFIG_H
