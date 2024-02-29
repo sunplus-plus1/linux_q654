dfbc32f33fd4adc1da8164d6e65774146626a78e
dx.jiang <dx.jiang@sunmedia.com.cn>
2024-02-29 18:17:18 +0800

    [DRM][display] 1. Support plane CROP/PAN function with HW OSD. 2. Add sp7350_osd_layer_set_by_region for OSD CROP/PAN function.

--- drivers/gpu/drm/sunplus/sp7350/sp7350_drm_plane.c
+++ drivers/gpu/drm/sunplus/sp7350/sp7350_drm_plane.c
@@ -164,7 +164,11 @@ static void sp7350_kms_plane_osd_atomic_update(struct drm_plane *plane,
 {
 	struct drm_plane_state *state = plane->state;
 	struct drm_gem_cma_object *obj = NULL;
+	#if 0
 	struct sp7350fb_info info;
+	#else
+	struct sp7350_osd_region info;
+	#endif
 	int osd_layer_sel = 0;
 	struct drm_format_name_buf format_name;
 
@@ -201,54 +205,98 @@ static void sp7350_kms_plane_osd_atomic_update(struct drm_plane *plane,
 		return;
 	}
 
+	memset(&info, 0, sizeof(info));
+	#if 0
 	info.color_mode = sp7350_get_format(state->fb->format->format, 0);
 	info.width = state->src_w >> 16;
 	info.height = state->src_h >> 16;
 	info.buf_addr_phy = (u32)obj->paddr;
+	sp7350_osd_layer_set(&info, osd_layer_sel);
+	#else
+	info.color_mode = sp7350_get_format(state->fb->format->format, 0);
+	info.buf_addr_phy = (u32)obj->paddr;
+	info.region_info.buf_width = state->fb->width;
+	info.region_info.buf_height = state->fb->height;
+	info.region_info.start_x = state->src_x >> 16;
+	info.region_info.start_y = state->src_y >> 16;
+	info.region_info.act_x = state->crtc_x;
+	info.region_info.act_y = state->crtc_y;
+	info.region_info.act_width = state->crtc_w;
+	info.region_info.act_height = state->crtc_h;
+
+	sp7350_osd_layer_set_by_region(&info, osd_layer_sel);
+	#endif
+
 
 	DRM_DEBUG_DRIVER("Pixel format %s, modifier 0x%llx, C3V format:0x%X\n",
 		      drm_get_format_name(state->fb->format->format, &format_name),
 		      state->fb->modifier, info.color_mode);
-
-	sp7350_osd_layer_set(&info, osd_layer_sel);
-
 	sp7350_dmix_layer_set(SP7350_DMIX_OSD0 + osd_layer_sel, SP7350_DMIX_BLENDING);
 }
 
-static int sp7350_kms_plane_atomic_check(struct drm_plane *plane,
+static int sp7350_kms_plane_vpp_atomic_check(struct drm_plane *plane,
 				   struct drm_plane_state *state)
 {
 	struct drm_crtc_state *crtc_state;
-	bool can_position = false;
 
 
-	if (!state->fb || WARN_ON(!state->crtc)){
-		DRM_DEBUG_DRIVER("%s return 0\n", __func__);
+	if (!state->fb || WARN_ON(!state->crtc)) {
+		DRM_DEBUG_DRIVER("return 0.\n");
 		return 0;
 	}
 
 	crtc_state = drm_atomic_get_crtc_state(state->state, state->crtc);
-	if (IS_ERR(crtc_state)){
-		DRM_DEBUG_DRIVER("%s is err\n", __func__);
+	if (IS_ERR(crtc_state)) {
+		DRM_DEBUG_DRIVER("drm_atomic_get_crtc_state is err\n");
 		return PTR_ERR(crtc_state);
-
 	}
-	if (plane->type == DRM_PLANE_TYPE_CURSOR)
-		can_position = true;
 
-	DRM_DEBUG_DRIVER("atomic check end\n");
+	if (plane->type == DRM_PLANE_TYPE_CURSOR) {
+		DRM_DEBUG_DRIVER("DRM_PLANE_TYPE_CURSOR unsupport for VPP HW.\n");
+		return -EINVAL;
+	}
+
+	DRM_DEBUG_DRIVER("plane atomic check end\n");
+
+	return 0;
+}
+
+static int sp7350_kms_plane_osd_atomic_check(struct drm_plane *plane,
+				   struct drm_plane_state *state)
+{
+	struct drm_crtc_state *crtc_state;
+
+
+	if (!state->fb || WARN_ON(!state->crtc)) {
+		DRM_DEBUG_DRIVER("return 0\n");
+		return 0;
+	}
+
+	crtc_state = drm_atomic_get_crtc_state(state->state, state->crtc);
+	if (IS_ERR(crtc_state)) {
+		DRM_DEBUG_DRIVER("drm_atomic_get_crtc_state is err\n");
+		return PTR_ERR(crtc_state);
+	}
+
+	if (state->crtc_w != state->src_w >> 16 || state->crtc_h != state->src_h >> 16) {
+		DRM_DEBUG_DRIVER("Check fail[src(%d, %d), crtc(%d,%d)], scale function unsuppord for OSD HW.\n",
+			state->src_w >> 16, state->src_h >> 16, state->crtc_w, state->crtc_h);
+		return -EINVAL;
+	}
+
+	DRM_DEBUG_DRIVER("plane atomic check end\n");
 
 	return 0;
 }
 
 static const struct drm_plane_helper_funcs sp7350_kms_vpp_helper_funcs = {
 	.atomic_update		= sp7350_kms_plane_vpp_atomic_update,
-	.atomic_check		=sp7350_kms_plane_atomic_check,
+	.atomic_check		=sp7350_kms_plane_vpp_atomic_check,
 };
 
 static const struct drm_plane_helper_funcs sp7350_kms_osd_helper_funcs = {
 	.atomic_update		= sp7350_kms_plane_osd_atomic_update,
-	.atomic_check		=sp7350_kms_plane_atomic_check,
+	.atomic_check		=sp7350_kms_plane_osd_atomic_check,
 };
 struct drm_plane *sp7350_drm_plane_init(struct drm_device *drm,
 				  enum drm_plane_type type, int index)
