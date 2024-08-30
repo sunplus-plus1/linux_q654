dd6469f5cdd012293a0893a0fc894f7cc159dbc3
chingchou.huang <chingchou.huang@sunplus.com>
2024-08-30 08:42:25 +0800

    [sound][soc] sunplus audio for kernel 6.6

---
+++ sound/soc/sunplus/spsoc_util.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0
+// ALSA SoC AUD7021 UTILITY MACRO
+//
+// Author: ChingChou Huang <chingchouhuang@sunplus.com>
+//
+//
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/delay.h>
+#include <linux/timer.h>
+#include "spsoc_util.h"
+
+//#define SYNCHRONIZE_IO		__asm__ __volatile__ ("dmb" : : : "memory")
+#define SYNCHRONIZE_IO	(__asm__ __volatile__ ("" : : : "memory"))
+
+void delay_ms(UINT32 ms_count)
+{
+//#if 0
+	// can not be used in interrupt (very important)
+//	msleep(ms_count);
+//#else
+	// cpu may buzy wait
+	mdelay(ms_count);
+//#endif
+}
+
+
+
+
